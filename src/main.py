eccfb7be4d627087469367177bbf69849830c816
CC server <watten@cc>
2023-07-05 16:05:22 +0200

    train

--- src/main.py
+++ src/main.py
@@ -11,16 +11,16 @@ if __name__ == "__main__":
     # mask_path = os.path.join(base_path, "training/groundtruth")
     # dataset = ETHDataset(image_path, mask_path, augment_images=False)
 
-    dataset = MassachusettsDataset('data/archive/tiff/train', 'data/archive/tiff/train_labels', augment_images=False)
+    #dataset = MassachusettsDataset('data/archive/tiff/train', 'data/archive/tiff/train_labels', augment_images=False)
 
-    # base_path = "data/additional_data"
-    # image_path = os.path.join(base_path, "images")
-    # mask_path = os.path.join(base_path, "masks")
-    # dataset = GMapsDataset(image_path, mask_path, augment_images=False)
+    base_path = "data/additional_data"
+    image_path = os.path.join(base_path, "images")
+    mask_path = os.path.join(base_path, "masks")
+    dataset = GMapsDataset(image_path, mask_path, augment_images=True)
 
     model = ResidualAttentionUNet(4, 1)
-    model.load_state_dict(torch.load('model_weights_2023-07-05_13.pth'))
+    model.load_state_dict(torch.load('model_weights_2023-07-05_14.pth'))
     loss_fn = DiceLoss(mode='binary')
-    trainer = Trainer(model, dataset, None, loss_fn, None, split_test=0.2, batch_size=4, epochs=10, test_metrics=[JaccardLoss(mode='binary'), loss_fn], test_metric_names=["JaccardLoss", "DiceLoss"], epochs_between_safe=10)
+    trainer = Trainer(model, dataset, None, loss_fn, None, split_test=0.2, batch_size=4, epochs=15, test_metrics=[JaccardLoss(mode='binary'), loss_fn], test_metric_names=["JaccardLoss", "DiceLoss"], epochs_between_safe=10)
     scores = trainer.train_test()
     scores.to_csv("test_scores.csv")
