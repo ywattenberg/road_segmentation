de17fd67b576ba327e1f945e4495d2aa53d0f35b
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-07-09 17:05:06 +0200

    dont use augmentation

--- src/feature_extractor.py
+++ src/feature_extractor.py
@@ -10,8 +10,8 @@ from trainer import Trainer
 
 transform = torchvision.transforms.Compose([
     torchvision.transforms.Resize((400, 400)),
-    torchvision.transforms.HorizontalFlip(),
-    torchvision.transforms.RandomRotation(20),
+    # torchvision.transforms.HorizontalFlip(),
+    # torchvision.transforms.RandomRotation(20),
     torchvision.transforms.ToTensor(),
     torchvision.transforms.Normalize(mean=[0.485, 0.456, 0.406],
                                     std=[0.229, 0.224, 0.225] )
