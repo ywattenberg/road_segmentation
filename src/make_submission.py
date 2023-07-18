458d370303557af7e9c7bf1ba2d536ce12c68764
Yannick Wattenberg <ywattenberg@eu-login-14.euler.ethz.ch>
2023-07-18 21:25:01 +0200

    merge

--- src/make_submission.py
+++ src/make_submission.py
@@ -4,15 +4,15 @@ import subprocess
 from torchvision import transforms
 from Dataset.dataset import ETHDataset
 from PIL import Image
-from Model.model import ResidualAttentionUNet
+from Model.model import ResidualAttentionUNet, ResidualAttentionDuckUNet
 from torchvision.utils import save_image
 
 if __name__ == "__main__":
-    base_path = "data/ethz-cil-road-segmentation-2023"
+    base_path = "data"
     image_path = os.path.join(base_path, "test/images")
-    pad = transforms.Compose([transforms.Pad(56, fill=(0)), transforms.ToTensor()])
-    model = ResidualAttentionUNet(4, 1)
-    model.load_state_dict(torch.load("best_model_weights_new_model.pth"))
+    pad =  transforms.Compose([transforms.Pad(56, fill=(0)), transforms.ToTensor()])
+    model = ResidualAttentionDuckUNet(4,1)
+    model.load_state_dict(torch.load("best_model_weights_pretrained_clDice_duck_final.pth"))
     model.eval()
     for file in os.listdir(image_path):
         print(file)
