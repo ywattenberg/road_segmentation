87a506a72d511c1bd20a34a4c1f79d0e9e68dec8
niedo <simon.wachter@outlook.com>
2023-07-18 15:20:16 +0200

    adjusted for smp models

--- src/make_submission.py
+++ src/make_submission.py
@@ -3,6 +3,7 @@ import os
 import subprocess
 import click
 
+import segmentation_models_pytorch as smp
 from torchvision import transforms
 from Dataset.dataset import ETHDataset
 from PIL import Image
@@ -15,15 +16,53 @@ from torchvision.utils import save_image
     "--model-name",
     "-m",
     help="Which model to use",
-    default="UnetPlusPlus-resnet34-imagenet-clDice",
+    default="UnetPlusPlus",
     type=str,
 )
-def main(model_name):
+@click.option(
+    "--encoder-name",
+    "-en",
+    help="Which encoder to use",
+    default="resnet34",
+    type=str,
+)
+@click.option(
+    "--encoder-weights",
+    "-ew",
+    help="Which encoder weights to use",
+    default="imagenet",
+    type=str,
+)
+@click.option(
+    "--loss",
+    "-l",
+    help="Which loss was used",
+    default="clDice",
+    type=str,
+)
+def main(model_name, encoder_name, encoder_weights, loss):
     base_path = "data/ethz-cil-road-segmentation-2023"
     image_path = os.path.join(base_path, "test/images")
     pad = transforms.Compose([transforms.Pad(56, fill=(0)), transforms.ToTensor()])
-    model = ResidualAttentionUNet(4, 1)
-    model.load_state_dict(torch.load(f"models/model_weights_{model_name}.pth"))
+
+    if model_name == "ResidualAttentionUNet":
+        model = ResidualAttentionUNet(4, 1)
+    else:
+        model_func = getattr(smp, model_name)
+
+        model = model_func(
+            encoder_name=encoder_name,
+            encoder_weights=encoder_weights,
+            # activation="sigmoid",
+            in_channels=4,
+            classes=1,
+        )
+    model.load_state_dict(
+        torch.load(
+            f"models/model_weights_{model_name}-{encoder_name}-{encoder_weights}-{loss}.pth",
+            map_location=torch.device("cpu"),
+        )
+    )
     model.eval()
     for file in os.listdir(image_path):
         print(file)
