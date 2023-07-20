448e9f92ca6526b0057fa871741700d7c127ade6
Yannick Wattenberg <wattenberg@ywattenberg.com>
2023-07-20 11:29:53 +0200

    change weight scheme

--- src/Loss/combined_loss.py
+++ src/Loss/combined_loss.py
@@ -3,18 +3,19 @@ from torch import nn
 from segmentation_models_pytorch import losses
 
 class DiceLovaszBCELoss(nn.Module):
-    def __init__(self, alpha=0.5, beta=0.3, tversky_alpha=0.5, tversky_beta=0.5, *args, **kwargs) -> None:
+    def __init__(self, alpha=0.5, beta=0.03, gamma=0.2, *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
         self.dice = losses.DiceLoss(mode="binary")
         self.lovasz = losses.LovaszLoss(mode="binary")
         self.BCE = nn.BCEWithLogitsLoss()
         self.alpha = alpha
         self.beta = beta
+        self.gamma = gamma
 
     def forward(self, y_pr, y_gt):
         dice = self.dice(y_pr, y_gt)
-        lovasz = self.lovasz(y_pr, y_gt)/10
+        lovasz = self.lovasz(y_pr, y_gt)
         bce = self.BCE(y_pr, y_gt)
         #print(f"dice: {dice}, lovasz: {lovasz}, bce: {bce}")
-        return  self.beta*bce + (1-self.beta)*(self.alpha * dice + (1 - self.alpha) * lovasz)
+        return  self.alpha * dice + self.beta * lovasz + self.gamma * bce
     
