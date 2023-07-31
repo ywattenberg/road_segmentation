f05ae0533599187d504bfbbd884ad757be856043
niedo <mail@siwachter.com>
2023-07-31 09:59:32 +0200

    small changes to plots

--- src/plot.py
+++ src/plot.py
@@ -14,14 +14,19 @@ def plot_model_comp(csv_dir="model-comparison"):
 
         df = pd.read_csv(os.path.join(csv_dir, file))
 
+        if file.split("-")[0] == "UnetPlusPlus":
+            model_name = "Unet++"
+        elif file.split("-")[0] == "DeepLabV3Plus":
+            model_name = "DeepLabV3+"
+
         df = df[df["epoch"] < 10]
-        plt.plot(df["epoch"], df["DiceLoss"], label=file.split("-")[0])
+        plt.plot(df["epoch"], df["DiceLoss"], label=model_name)
     plt.legend()
     plt.tight_layout(pad=3)
     plt.title("Different models with resnet34 encoder")
     plt.xlabel("Epoch")
     plt.ylabel("Dice Loss")
-    fig.savefig(f"figures/model-comparison-10.png", dpi=1000)
+    fig.savefig(f"figures/model-comparison-10.png", dpi=300)
 
 
 def plot_encoder_comp_unetpp(csv_dir="encoder-comparison"):
@@ -31,16 +36,21 @@ def plot_encoder_comp_unetpp(csv_dir="encoder-comparison"):
         if not file.endswith(".csv") or not file.startswith("Unet"):
             continue
 
+        if file.split("-")[1] != "efficientnet":
+            label = f"{file.split('-')[1]}"
+        else:
+            label = f"{file.split('-')[1]}-{file.split('-')[2]}"
+
         df = pd.read_csv(os.path.join(csv_dir, file))
         df = df[df["epoch"] < 30]
-        plt.plot(df["epoch"], df["DiceLoss"], label=file.split("-")[1])
+        plt.plot(df["epoch"], df["DiceLoss"], label=label)
     # plt.ylim(0.1, 0.4)
     plt.legend()
     plt.tight_layout(pad=3)
-    plt.title("UnetPlutPlus model with different encoders")
+    plt.title("Unet++ model with different encoders")
     plt.xlabel("Epoch")
     plt.ylabel("Dice Loss")
-    fig.savefig(f"figures/encoder-comparison-unetpp-30.png", dpi=1000)
+    fig.savefig(f"figures/encoder-comparison-unetpp-30.png", dpi=300)
 
 
 def plot_model_encoder_comp(csv_dir="encoder-comparison"):
@@ -56,10 +66,15 @@ def plot_model_encoder_comp(csv_dir="encoder-comparison"):
         df = pd.read_csv(os.path.join(csv_dir, file))
         df = df[df["epoch"] < 30]
 
+        if file.split("-")[0] == "UnetPlusPlus":
+            model_name = "Unet++"
+        elif file.split("-")[0] == "DeepLabV3Plus":
+            model_name = "DeepLabV3+"
+
         if file.split("-")[1] != "efficientnet":
-            label = f"{file.split('-')[0]}, {file.split('-')[1]}"
+            label = f"{model_name}, {file.split('-')[1]}"
         else:
-            label = f"{file.split('-')[0]}, {file.split('-')[1]}-{file.split('-')[2]}"
+            label = f"{model_name}, {file.split('-')[1]}-{file.split('-')[2]}"
         plt.plot(
             df["epoch"],
             df["DiceLoss"],
@@ -70,7 +85,7 @@ def plot_model_encoder_comp(csv_dir="encoder-comparison"):
     # plt.title("Dice Loss / Epoch for UnetPlutPlus model with various encoders")
     plt.xlabel("Epoch")
     plt.ylabel("Dice Loss")
-    fig.savefig(f"figures/model-encoder-comparison-30.png", dpi=1000)
+    fig.savefig(f"figures/model-encoder-comparison-30.png", dpi=300)
 
 
 def plot_final_models(csv_dir="final-models"):
@@ -79,10 +94,15 @@ def plot_final_models(csv_dir="final-models"):
     for file in sorted(os.listdir(csv_dir)):
         df = pd.read_csv(os.path.join(csv_dir, file))
 
+        if file.split("-")[0] == "UnetPlusPlus":
+            model_name = "Unet++"
+        elif file.split("-")[0] == "DeepLabV3Plus":
+            model_name = "DeepLabV3+"
+
         if file.split("-")[1] != "efficientnet":
-            label = f"{file.split('-')[0]}, {file.split('-')[1]}"
+            label = f"{model_name}, {file.split('-')[1]}"
         else:
-            label = f"{file.split('-')[0]}, {file.split('-')[1]}-{file.split('-')[2]}"
+            label = f"{model_name}, {file.split('-')[1]}-{file.split('-')[2]}"
 
         plt.plot(df["epoch"], df["DiceLoss"], label=label)
     plt.legend()
@@ -90,11 +110,11 @@ def plot_final_models(csv_dir="final-models"):
     # plt.title("Dice Loss / Epoch for UnetPlutPlus model with various encoders")
     plt.xlabel("Epoch")
     plt.ylabel("Dice Loss")
-    fig.savefig(f"figures/final-models.png", dpi=1000)
+    fig.savefig(f"figures/final-models.png", dpi=300)
 
 
 if __name__ == "__main__":
-    # plot_model_comp()
-    # plot_encoder_comp_unetpp()
-    # plot_model_encoder_comp()
+    plot_model_comp()
+    plot_encoder_comp_unetpp()
+    plot_model_encoder_comp()
     plot_final_models()
